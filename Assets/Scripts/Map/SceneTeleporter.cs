1e1fc219021dca3de50a0d4071099f53f045cadc
Youngwoo Choi <cyan246010@gmail.com>
2025-03-30 05:08:06 -0600

    Spawn point fix

--- Assets/Scripts/Map/SceneTeleporter.cs
+++ Assets/Scripts/Map/SceneTeleporter.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace EasyTransition
 {
@@ -7,15 +8,33 @@ namespace EasyTransition
         public TransitionSettings transition;
         public float startDelay;
         private PlayerController playerController;
+        private PodScript podScript;
 
         void Awake()
         {
             playerController = PlayerController.Instance;
+            podScript = PodScript.Instance;
         }
 
         public void LoadScene(string _sceneName)
         {
+            SceneManager.sceneLoaded += OnSceneLoaded;
             TransitionManager.Instance().Transition(_sceneName, transition, startDelay);
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+
+            if (playerController != null)
+            {
+                playerController.transform.position = new Vector3(0f, -2.5f, 0f);
+            }
+
+            if (podScript != null)
+            {
+                podScript.transform.position = new Vector3(0f, -2.5f, 0f);
+            }
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
