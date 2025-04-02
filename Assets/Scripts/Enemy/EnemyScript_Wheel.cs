a091dd1bd835758181395e1976ff2fcd394caa52
Youngwoo Choi <cyan246010@gmail.com>
2025-04-02 14:35:44 -0600

    New enemy wheel

---
+++ Assets/Scripts/Enemy/EnemyScript_Wheel.cs
@@ -0,0 +1,151 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyScript_Wheel : EnemyBase
+{
+    // Movement
+    private Vector3 moveDirection;
+    private float moveSpeed;
+    private float detectionRange;
+   
+    private Transform player;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private AudioManager _audio;
+
+    // Wheel to spin
+    [SerializeField] Transform wheel;
+    private float wheelRotationSpeed = 720f;
+    private float damage = 5f;
+    private float damageCooldown = 1f;
+    private float lastDamageTime = -999f;
+
+    [SerializeField] GameObject explosion;
+    [SerializeField] GameObject sparkEffect;
+    [SerializeField] Transform sparkPos;
+
+    void Start()
+    {
+        _audio = AudioManager.Instance;
+        isalive = true;
+
+        // Enemy stat
+        resourceAmount = 50;
+        maxHealth = 50;
+        moveSpeed = 8f;
+        currentHealth = maxHealth;
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        if(playerController != null)
+        {
+            player = playerController.GetAimPos();
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            moveDirection = new Vector3(Mathf.Sign(dirToPlayer.x), 0, 0);
+            FlipSprite(moveDirection.x);
+        }
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 1f; 
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (sparkEffect != null && sparkPos != null)
+        {
+            GameObject effect = Instantiate(sparkEffect, sparkPos.position, Quaternion.identity, sparkPos);
+        }
+    }
+
+
+    void Update()
+    {
+        if (!isalive)
+            return;
+
+        if (CheckWallCollision())
+        {
+            ChangeDirection();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!isalive)
+            return;
+
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, rb.linearVelocity.y);
+        RotateWheel();
+    }
+
+    private void RotateWheel()
+    {
+        if(wheel == null)
+        {
+            return;
+        }
+        float direction = Mathf.Sign(moveDirection.x);
+        wheel.Rotate(Vector3.forward * -direction * wheelRotationSpeed * Time.deltaTime);
+    }
+
+    private bool CheckWallCollision()
+    {
+        Vector2 front = transform.position + new Vector3(moveDirection.x * 1.2f, 0, 0);
+        return Physics2D.Linecast(transform.position, front, LayerMask.GetMask("Terrain"));
+    }
+
+
+    private void ChangeDirection()
+    {
+        moveDirection.x = -moveDirection.x;
+        FlipSprite(moveDirection.x);
+    }
+
+    private void FlipSprite(float directionX)
+    {
+        if (directionX != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * Mathf.Sign(directionX), transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Hitbox_Player"))
+        {
+            if (Time.time - lastDamageTime < damageCooldown)
+            {
+                return;
+            }
+
+            PlayerController playerController = other.GetComponentInParent<PlayerController>();
+            if (playerController != null && playerController.IsAlive() && !playerController.GetPlayerInvincible())
+            {
+                playerController.Hurt(damage);
+                lastDamageTime = Time.time;
+            }
+        }
+    }
+
+
+    public override void TakeDamage(float damage)
+    {
+        if (!isalive)
+            return;
+
+        base.TakeDamage(damage);
+        StartCoroutine(DamageFlash());
+    }
+
+    private IEnumerator DamageFlash()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
+    }
+
+    protected override void Die(int amount)
+    {
+        isalive = false;
+        Vector3 explosionPos = transform.position;
+        GameObject explosionInstance = Instantiate(explosion, explosionPos, Quaternion.identity);
+        _audio.PlayOneShot(_audio.Explosion, transform.position);
+        Destroy(explosionInstance.gameObject, 5f);
+        base.Die(amount);
+    }
+}
