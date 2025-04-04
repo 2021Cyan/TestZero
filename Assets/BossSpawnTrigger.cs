163e6670407d2793c09111523127cc8f0e0ac455
Youngwoo Choi <cyan246010@gmail.com>
2025-04-04 17:58:55 -0600

    bossfight wip

--- Assets/BossSpawnTrigger.cs
+++ Assets/BossSpawnTrigger.cs
@@ -5,14 +5,16 @@ public class BossSpawnTrigger : MonoBehaviour
     // Attributes
     public GameObject BossEnemy;
     public Transform BossSpawnPoint;
+    private bool hasSpawned = false;
 
     // Behaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         // Spawn boss if player enters hitbox
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !hasSpawned)
         {
             Instantiate(BossEnemy, BossSpawnPoint.position, Quaternion.identity);
+            hasSpawned = true;
         }
     }
 }
